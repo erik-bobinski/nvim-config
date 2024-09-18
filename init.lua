e672a25cc86f4a8534ef81dde093854bf945c663
Erik <123090127+erik-bobinski@users.noreply.github.com>
2024-09-18 19:58:36 -0400

    config updated with lazy plugin manager

--- init.lua
+++ init.lua
@@ -1,23 +1,4 @@
-require("erik.plugins-setup")
-require("erik.core.options")
-require("erik.core.keymaps")
-require("erik.core.colorscheme")
--- Enable transparency in neovim
-vim.cmd("highlight Normal guibg=NONe ctermbg=NONE")
-vim.cmd("highlight NonText guibg=NONE ctermbg=NONE")
+require("erik.core")
+require("erik.lazy")
 
--- Transparent background for the cursor line
-vim.cmd("highlight CursorLine guibg=NONE ctermbg=NONE")
-
--- Transparent background for the line numbers
-vim.cmd("highlight LineNr guibg=NONE ctermbg=NONE")
-
--- Transparent background for the current line number (the line with the cursor)
-vim.cmd("highlight CursorLineNr guibg=NONE ctermbg=NONE")
-
--- Transparent background for the sign column (gutter)
-vim.cmd("highlight SignColumn guibg=NONE ctermbg=NONE")
-
--- Transparent background for the fold column (if you use folding)
-vim.cmd("highlight FoldColumn guibg=NONE ctermbg=NONE")
 
--- lua/erik/core/options.lua
+++ lua/erik/core/options.lua
@@ -1,38 +1,36 @@
-local opt = vim.opt -- for conciseness
+vim.cmd("let g:netrw_liststyle = 3")
 
--- line numbers
+local opt = vim.opt
+
 opt.relativenumber = true
 opt.number = true
 
--- tabs & indentation
-opt.tabstop = 4
-opt.shiftwidth = 4
-opt.expandtab = true
-opt.autoindent = true
+-- tabs & indenation
+opt.tabstop = 4 -- 4 spaces for tabs
+opt.shiftwidth = 4 -- 4 spaces for indent width
+opt.expandtab = true -- expand tab to spaces
+opt.autoindent = true -- copy indent from current line when starting new one
 
--- line wrapping
-opt.wrap = false
+opt.wrap = false -- no text wrapping
 
 -- search settings
 opt.ignorecase = true
-opt.smartcase = true
+opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
 
--- cursor line
 opt.cursorline = true
 
--- appearance
+-- turn on termguicolors for tokyonight colorscheme to work
+-- (have to use iterm2 or any other true color terminal)
 opt.termguicolors = true
-opt.background = "dark"
-opt.signcolumn = "yes"
+opt.background = "dark" -- colorscheme that can be light or dark default to dark
+opt.signcolumn = "yes" -- show sign column so that text doesn't shift
 
--- backspace
-opt.backspace = "indent,eol,start"
+--backspace
+opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
 
--- clipboard
-opt.clipboard:append("unnamedplus")
+--clipboard
+opt.clipboard:append("unnamedplus") -- use system clipboard as default register
 
 -- split windows
-opt.splitright = true
-opt.splitbelow = true
-
-opt.iskeyword:append("-")
+opt.splitright = true -- split vertical window to the right
+opt.splitbelow = true -- split horizontal window to the bottom
